e2989f7a3526b831f7fac9907d363722df4bfbc8
Vincent Falcone <vincent.falcone@pieinsurance.com>
2021-12-02 11:46:01 -0500

    day 2

---
+++ day2/part2.py
@@ -0,0 +1,19 @@
+from input_parser import parse_input
+
+in_ = parse_input("day2/part1.txt")
+
+horizontal = 0
+depth = 0
+aim = 0
+
+for command in in_:
+    split_command = command.split(' ')
+    if split_command[0] == "forward":
+        horizontal += int(split_command[1])
+        depth += aim * int(split_command[1])
+    elif split_command[0] == "up":
+        aim -= int(split_command[1])
+    elif split_command[0] == "down":
+        aim += int(split_command[1])
+
+print(horizontal, depth, horizontal*depth)
--- input_parser.py
+++ input_parser.py
@@ -1,4 +1,4 @@
 def parse_input(file_path):
     with open(file_path) as file:
-        in_ = file.read().split()
+        in_ = file.read().split('\n')
     return in_
